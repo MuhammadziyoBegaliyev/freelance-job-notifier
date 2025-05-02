36d5a21392424b958dccc8bee4ce60c7daf1f823
MuhammadziyoBegaliyev <muhammadziyobegaliyev872@gmail.com>
2025-05-02 22:13:13 +0500

    blogProject

--- blogProject/blog/views.py
+++ blogProject/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView #create new views , madellarga o`zgartirish kiritadi 
+from django.urls import reverse_lazy #madel ochirilgandan song ishga tushadi
 from .models import Post
 # Create your views here.
 
@@ -12,3 +14,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
